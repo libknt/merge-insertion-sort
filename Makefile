125c75d3089e30a3bef24126a87c43b4c72ce2ff
libknt <101494707+key-door@users.noreply.github.com>
2023-09-15 16:57:18 +0900

    refactor

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
 all:
 	c++ -std=c++98 main.cpp && ./a.out
 
+w:
+	c++ -Wall -Werror -Wextra -std=c++98 main.cpp && ./a.out
+
--- main.cpp
+++ main.cpp
@@ -23,9 +23,9 @@ int jacobsthalNumber(int n) {
 void mergeInsertionSort(std::vector < int > & v, int start, int end, int pairSize);
 
 int comparison(int pairSize, std::vector < int > & v, int start) {
-    if (v.size() <= (pairSize + start))
+    if ((int)v.size() <= (pairSize + start))
         return -1;
-    std::cout << v[start] << "<" << v[pairSize + start] << std::endl;
+    // std::cout << v[start] << "<" << v[pairSize + start] << std::endl;
     if (v[start] < v[pairSize + start])
         return 1;
     return 0;
@@ -39,7 +39,7 @@ void swap(int pairSize, std::vector < int > & v, int start) {
 }
 
 int checkOverflow(int pairSize, std::vector < int > & v, int start) {
-    size_t size = v.size() - 1;
+    int size = v.size() - 1;
 
     for (int i = 0; i < pairSize; ++i) {
         if (start + pairSize > size)
@@ -51,7 +51,7 @@ int checkOverflow(int pairSize, std::vector < int > & v, int start) {
 
 void pairwiseComparison(int pairSize, std::vector < int > & v, int start, int end) {
 
-    std::cout << std::endl << "pairsize: " << pairSize << std::endl;
+    (void)start;
     for (int i = 0; i < end;) {
         if (checkOverflow(pairSize, v, i))
             break;
@@ -101,92 +101,21 @@ void separateMainChainAndSubChain(std::vector < int > & v, std::vector < int > &
     }
 }
 
-void insertAtTheStart(std::vector < int > & v, std::vector < int > & s, v_itr begin, v_itr end) {
-    v.insert(v.begin(), begin, end);
-    // s.erase(begin, end);
+void insertAtTheStart(std::vector < int > & v,v_itr firstKey) {
+    v.insert(v.begin(), *firstKey);
 }
 
 void insert(std::vector < int > & v, v_itr position, int key) {
     v.insert(position, key);
 }
 
-// void insert(std::vector<int> & v, v_itr position, std::vector<int> & s, v_itr begin, v_itr end) {
-//     if(begin == end){
-//         v.insert(position, *begin);
-//         // s.erase(begin);
-//         return;
-//     }
-//     v.insert(position, begin, end);
-//     // s.erase(begin, end);
-// }
-
-// bool isKey(std::vector<v_itr> iterators, int index, int key) {
-//     if (*(iterators[index]) >= key) return true;
-//     else return false;
-// }
-
-// v_itr lower_bound(std::vector<v_itr>& iterators, vi_itr begin, vi_itr end, int key, int right) {
-//     int left = -1;
-//     while (right - left > 1) {
-//         int mid = left + (right - left) / 2;
-
-//         if (isKey(iterators, mid, key)) right = mid;
-//         else left = mid;
-//     }
-
-//     return *(iterators.begin() + right);
-// }
-
-// v_itr   binarySearch(std::vector<int> & v, int key, int pairSize, int keyPosition) {
-//     std::vector<v_itr> iterators;
-
-//     for (v_itr itr = v.begin(); itr != v.end(); itr += pairSize) {
-//         iterators.push_back(itr);
-//     }
-//     vi_itr begin = iterators.begin();
-//     vi_itr end = iterators.begin() + keyPosition;
-//     return lower_bound(iterators,begin, end, key, iterators.size());
-// }
-
 bool isKey(std::vector < int > & v, int index, int key) {
     if (v[index] >= key) return true;
     else return false;
 }
 
-// int binarySearch(std::vector<int> & v, int key, int right) {
-//     int left = -1;
-
-//     std::cout << std::endl << "right   " << v[right] << std::endl;
-//     while (right - left > 1) {
-//         int mid = left + (right - left) / 2;
-
-//         if (isKey(v, mid, key)) right = mid;
-//         else left = mid;
-//     }
-
-//     return right;
-// }
-
 v_itr binarySearch(std::vector < int > & v, int key, int right) {
-    // int left = -1;
-
-    // std::cout << std::endl << "right   " << v[right] << std::endl;
-    // while (right - left > 1) {
-    //     int mid = left + (right - left) / 2;
-
-    //     if (isKey(v, mid, key)) right = mid;
-    //     else left = mid;
-    // }
-    // v_itr it = v.begin();
-    // for ( int i=0; i <= right; ++i ) {
-    //     ++it;
-    // }
-    std::cout << std::endl << "key " << key << "  it   " << * (v.begin() + right + 1) << std::endl;
-
     return std::lower_bound(v.begin(), v.begin() + right + 1, key);
-    // return std::upper_bound(v.begin(), it, key);
-
-    // return right;
 }
 
 void insertionFromSubIntoMain(std::vector < int > & v, std::vector < int > & s, int pairSize) {
@@ -194,11 +123,10 @@ void insertionFromSubIntoMain(std::vector < int > & v, std::vector < int > & s, 
     int jac;
     v_itr itr;
     int insertNum = 0;
-    int i = 0;
     while (1) {
         jac = jacobsthalNumber(n);
 
-        v_itr s_begin = s.begin();
+        // v_itr s_begin = s.begin();
         v_itr s_end = s.begin();
         for (int i = 0; i < pairSize; ++i) {
             ++s_end;
@@ -207,31 +135,16 @@ void insertionFromSubIntoMain(std::vector < int > & v, std::vector < int > & s, 
             }
         }
         int index = jac;
-        if (s.size() < jac)
+        if ((int)s.size() < jac)
             index = s.size();
-        std::cout << std::endl;
         for (; jacobsthalNumber(n - 1) < index;) {
-
-            std::cout << std::endl << "vector" << std::endl;
-            i = 0;
-            for (v_itr it = v.begin(); it != v.end(); ++it) {
-                if ((i % pairSize) == 0)
-                    std::cout << "[";
-                std::cout << * it;
-                if ((i % pairSize) == 0)
-                    std::cout << "]";
-                std::cout << " ";
-                i++;
-            }
             itr = binarySearch(v, s[index - 1], insertNum + index - 1);
-            std::cout << "^" << s[index - 1] << " " << * itr << "^";
             insert(v, itr, s[index - 1]);
             ++insertNum;
             --index;
         }
-        std::cout << std::endl;
         ++n;
-        if (s.size() < jac) break;
+        if ((int)s.size() < jac) break;
     }
 
 }
@@ -244,50 +157,33 @@ void mergeInsertionSort(std::vector < int > & v, int start, int end, int pairSiz
     // mergeInsertionSort(v, 0, v.size(), pairSize * 2);
     // if(pairSize < 4)
     //     return;
-    std::cout << "size " << v.size() << std::endl;
 
     std::vector < int > s;
-    int i = 0;
-    // std::cout << std::endl <<  "=========================" << std::endl;
-    //     std::cout << std::endl <<  "vector" << std::endl;
-    //     i = 0;
-    //     for ( v_itr it = v.begin(); it != v.end(); ++it) {
-    //         if((i % pairSize) == 0)
-    //             std::cout << "[";
-    //         std::cout << *it;
-    //         if((i % pairSize) == 0)
-    //             std::cout << "]";
-    //         std::cout << " ";
-    //         i++;
-    //     }
     separateMainChainAndSubChain(v, s, pairSize);
-    std::cout << std::endl << "vector" << std::endl;
-    i = 0;
-    for (v_itr it = v.begin(); it != v.end(); ++it) {
-        if ((i % pairSize) == 0)
-            std::cout << "[";
-        std::cout << * it;
-        if ((i % pairSize) == 0)
-            std::cout << "]";
-        std::cout << " ";
-        i++;
-    }
-    std::cout << std::endl << "small" << std::endl;
-    i = 0;
-    for (v_itr it = s.begin(); it != s.end(); ++it) {
-        if ((i % pairSize) == 0)
-            std::cout << "[";
-        std::cout << * it;
-        if ((i % pairSize) == 0)
-            std::cout << "]";
-        std::cout << " ";
-        i++;
-    }
-    std::cout << std::endl << "=========================" << std::endl;
-    insertAtTheStart(v, s, s.begin(), s.begin() + pairSize);
-    std::cout << std::endl << "=========================" << std::endl;
-    std::cout << "                                      pairSize: " << pairSize << std::endl;
-    std::cout << std::endl << "[][][][][][][][][][][][]]" << std::endl;
+    // int i = 0;
+    // std::cout << std::endl << "vector" << std::endl;
+    // i = 0;
+    // for (v_itr it = v.begin(); it != v.end(); ++it) {
+    //     if ((i % pairSize) == 0)
+    //         std::cout << "[";
+    //     std::cout << * it;
+    //     if ((i % pairSize) == 0)
+    //         std::cout << "]";
+    //     std::cout << " ";
+    //     i++;
+    // }
+    // std::cout << std::endl << "small" << std::endl;
+    // i = 0;
+    // for (v_itr it = s.begin(); it != s.end(); ++it) {
+    //     if ((i % pairSize) == 0)
+    //         std::cout << "[";
+    //     std::cout << * it;
+    //     if ((i % pairSize) == 0)
+    //         std::cout << "]";
+    //     std::cout << " ";
+    //     i++;
+    // }
+    insertAtTheStart(v, s.begin());
     insertionFromSubIntoMain(v, s, pairSize);
     // std::cout << std::endl <<  "vector" << std::endl;
     // for ( v_itr it = v.begin(); it != v.end(); ++it) {
@@ -318,7 +214,6 @@ void mergeInsertionSort(std::vector < int > & v, int start, int end, int pairSiz
     //     std::cout << *it << " ";
     // }
     // std::cout << std::endl <<  "=========================" << std::endl;
-    std::cout << "size " << v.size() << std::endl;
 }
 
 void mergeInsertionSortAlgorithm(std::vector < int > & v) {
@@ -371,7 +266,7 @@ int main() {
 
     mergeInsertionSortAlgorithm(v);
     // int i= 0;
-    int pairSize = 8;
+    // int pairSize = 8;
     for (v_itr it = v.begin(); it != v.end(); ++it) {
         // if((i % pairSize) == 0)
         // std::cout << "[";
